e492a35448fe82bd272641f2ea8c0428f06751e4
WALID EL ALAOUY <w.elalaouy@ta3meed.com>
2024-11-06 16:28:55 +0100

    add tf files

---
+++ src/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_location" {
+  default     = "West Europe"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "walid-tf-rg"
+  description = "Name of the resource group."
+}
+
